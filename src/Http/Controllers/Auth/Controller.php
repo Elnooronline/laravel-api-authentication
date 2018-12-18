e1a58fadec436583faf3b75128e2e678f26f4f34
Ahmed Fathy <aliraqi.dev@gmail.com>
2018-12-18 19:57:29 +0200

    remove unused imports

--- src/Http/Controllers/Auth/Controller.php
+++ src/Http/Controllers/Auth/Controller.php
@@ -7,11 +7,10 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
-use Elnooronline\LaravelConcerns\Http\Controllers\Helpers;
 
 class Controller extends BaseController
 {
-    use AuthorizesRequests, DispatchesJobs, ValidatesRequests, Helpers;
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 
     /**
      * @param \Illuminate\Http\Request $request
