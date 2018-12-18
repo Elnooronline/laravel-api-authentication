a5ed2252ca9e7861f7570be2ccfbc567766daf60
Ahmed Fathy <aliraqi.dev@gmail.com>
2018-12-18 19:58:34 +0200

    install unit test

---
+++ tests/TestCase.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Tests;
+
+use Orchestra\Testbench\TestCase as OrchestraTestCase;
+use Elnooronline\LaravelApiAuthentication\Providers\ServiceProvider;
+
+class TestCase extends OrchestraTestCase
+{
+    /**
+     * Setup the test environment.
+     */
+    protected function setUp()
+    {
+        parent::setUp();
+
+        $this->loadLaravelMigrations(['--database' => 'testbench']);
+
+        $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
+    }
+
+    /**
+     * Load package service provider
+     *
+     * @param  \Illuminate\Foundation\Application $app
+     * @return array
+     */
+    protected function getPackageProviders($app)
+    {
+        return [
+            ServiceProvider::class,
+        ];
+    }
+
+    /**
+     * Define environment setup.
+     *
+     * @param  \Illuminate\Foundation\Application $app
+     * @return void
+     */
+    protected function getEnvironmentSetUp($app)
+    {
+        // Setup default database to use sqlite :memory:
+        $app['config']->set('database.default', 'testbench');
+        $app['config']->set('database.connections.testbench', [
+            'driver' => 'sqlite',
+            'database' => ':memory:',
+            'prefix' => '',
+        ]);
+    }
+}
---
+++ tests/Unit/AuthenticationUnitTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Tests\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class AuthenticationUnitTest extends TestCase
+{
+    public function test_login()
+    {
+        User::create([
+            'name' => 'Elnooronline',
+            'email' => 'admin@elnooronline.com',
+            'password' => Hash::make('secret'),
+        ])->markEmailAsVerified();
+
+        $response = $this->postJson('/api/login', []);
+        $response->assertStatus(422);
+        $response->assertJsonStructure([
+            'errors' => ['email', 'password']
+        ]);
+    }
+}
