0191b6e6b199ef26324c33c66301f1f9716f32bd
Ahmed Al-Shahawi <alshahawi@outlook.com>
2019-03-24 15:01:47 +0200

    Update lang lines

--- resources/lang/en/passwords.php
+++ resources/lang/en/passwords.php
@@ -1,4 +1,14 @@
 <?php
+
 return [
-    'email-message' => 'The verification code for reset password is :code',
+    'wrong-code' => 'The code you have entered is invalid.',
+    'wrong-token' => 'The token you have entered is invalid',
+    'email-message' => 'Password reset code is: :code',
+    'attributes' => [
+        'name' => 'Name',
+        'email' => 'Email Address',
+        'code' => 'Code',
+        'password' => 'Password',
+        'token' => 'Token',
+    ],
 ];
